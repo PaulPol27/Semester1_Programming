1699588909c6ab2005a18eb504db5fe53b8c64d0
PaulPol27 <pablopol911@gmail.com>
2019-09-26 11:48:19 +0300

    HW_1 done

--- hwproblemsolving1/hwproblemsolving1/problemsolvinghw1.cpp
+++ hwproblemsolving1/hwproblemsolving1/problemsolvinghw1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <random>
 using namespace std;
 
 void numb8g() {
@@ -34,9 +35,83 @@ void numb12() {
 
 }
 
+void num_14(){
+	const int n = 80;
+	int lower = 0;
+	int upper = 10000;
+	double arr[n];
+	double min = DBL_MAX;
+	int position;
+	cout << min << endl;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = (double)(rand() % (upper - (lower)+1) + (lower)) / 1000;
+	}
+	for (int i = 0; i < n; i++) {
+		int a = (int)arr[i];
+		int b = a + 1;
+		double dist_1 = b - arr[i];
+		double dist_2 = arr[i] - a;
+		double distance;
+		if (dist_1 > dist_2)
+		{
+			distance = dist_2;
+		}
+		else {
+			distance = dist_1;
+		}
+		if (distance < min) {
+			min = distance;
+			position = i;
+		}
+		cout << min << endl;
+	}
+	cout << position << " " << arr[position] << " " << min << endl;
+}
+
+void num_17() {
+	int n;
+	cin >> n;
+	double sum = 0;
+	int lower = 0;
+	int upper = 10000;
+	double *arr= new double[n];
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = (double)(rand() % (upper - (lower)+1) + (lower)) / 1000;
+		cout << arr[i] << endl;
+	}
+	int k = n;
+	for (int i = 0; i < n; i++) {
+		sum += k * arr[i];
+		k -= 1;
+	}
+	cout << "Sum = " << sum << endl;
+}
+
+void num_18() {
+	const int n = 200;
+	double arr[n];
+	int lower = 0;
+	int upper = 10000;
+	for (int i = 0; i < n; i++) {
+		arr[i] = (double)(rand() % (upper - (lower)+1) + (lower)) / 1000;
+	}
+	int count = 0;
+	for (int i = 1; i < n - 1; i++) {
+		if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
+			count++;
+		}
+	}
+	cout << "Count = " << count << endl;
+}
+
 int main() {
-	numb8g();
+	//numb8g();
 	//numb12();
+	num_14();
+	//num_17();
+	//num_18();
 	system("pause");
 	return 0;
 }
